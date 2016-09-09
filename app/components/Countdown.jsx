118b3a9e2bd96db08dfc69dc0c87b68afae28d36
Jose Pablo Madrigal <jose.pablo.madrigal@firstfactory.com>
2016-09-09 09:09:48 -0600

    Added tests and clock styles and format

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,8 +1,10 @@
 var React = require('react');
-
+var Clock = require('Clock');
 var Countdown = () => {
   return (
-    <div>countdown.jsx</div>
+    <div>
+      <Clock totalSeconds={129}/>
+    </div>
   );
 };
 
